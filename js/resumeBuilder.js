284dd4a9107b54013f67d1318e183b46d71535ed
Guillermo García López <memowii@hotmail.com>
2017-02-02 17:15:26 -0600

    Change for loops

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -96,38 +96,38 @@ var education = {
         }
     ],
     display: function () {
-        for (var school in this.schools) {
+        this.schools.forEach(function (school) {
             $("#education").append(HTMLschoolStart);
 
-            var formattedSchoolName = HTMLschoolName.replace("%data%", this.schools[school].name);
-            var formattedSchoolDegree = HTMLschoolDegree.replace("%data%", this.schools[school].degree);
+            var formattedSchoolName = HTMLschoolName.replace("%data%", school.name);
+            var formattedSchoolDegree = HTMLschoolDegree.replace("%data%", school.degree);
             $(".education-entry:last").append(formattedSchoolName+formattedSchoolDegree);
 
-            var formattedDates = HTMLschoolDates.replace("%data%", this.schools[school].dates);
+            var formattedDates = HTMLschoolDates.replace("%data%", school.dates);
             $(".education-entry:last").append(formattedDates);
 
-            for (major in this.schools[school].majors){
-                var formattedSchoolMajor = HTMLschoolMajor.replace("%data%", this.schools[school].majors[major]);
+            school.majors.forEach(function (major) {
+                var formattedSchoolMajor = HTMLschoolMajor.replace("%data%", major);
                 $(".education-entry:last").append(formattedSchoolMajor);
-            }
-            var formattedSchoolLocation = HTMLschoolLocation.replace("%data%", this.schools[school].location);
+            });
+            var formattedSchoolLocation = HTMLschoolLocation.replace("%data%", school.location);
             $(".education-entry:last").append(formattedSchoolLocation);
-        }
+        });
 
         $("#education").append(HTMLonlineClasses);
-        for (var onlineCourse in this.onlineCourses) {
+        this.onlineCourses.forEach(function (onlineCourse) {
             $("#education").append(HTMLschoolStart);
 
-            var formattedOnlineTittle = HTMLonlineTitle.replace("%data%", this.onlineCourses[onlineCourse].title);
-            var formattedOnlineSchool = HTMLonlineSchool.replace("%data%", this.onlineCourses[onlineCourse].school);
+            var formattedOnlineTittle = HTMLonlineTitle.replace("%data%", onlineCourse.title);
+            var formattedOnlineSchool = HTMLonlineSchool.replace("%data%", onlineCourse.school);
             $(".education-entry:last").append(formattedOnlineTittle+formattedOnlineSchool);
 
-            var formattedOnlineDates = HTMLonlineDates.replace("%data%", this.onlineCourses[onlineCourse].dates);
+            var formattedOnlineDates = HTMLonlineDates.replace("%data%", onlineCourse.dates);
             $(".education-entry:last").append(formattedOnlineDates);
 
-            var formattedOnlineURL = HTMLonlineURL.replace("%data%", this.onlineCourses[onlineCourse].url);
+            var formattedOnlineURL = HTMLonlineURL.replace("%data%", onlineCourse.url);
             $(".education-entry:last").append(formattedOnlineURL);
-        }
+        });
     }
 };
 
@@ -149,21 +149,22 @@ var work = {
         }
     ],
     display: function () {
-        for (var job in this.jobs) {
+        this.jobs.forEach(function (job) {
             $("#workExperience").append(HTMLworkStart);
 
-            var formattedEmployer = HTMLworkEmployer.replace("%data%", this.jobs[job].employer);
-            var formattedTitle= HTMLworkTitle.replace("%data%", this.jobs[job].title);
+            var formattedEmployer = HTMLworkEmployer.replace("%data%", job.employer);
+            var formattedTitle= HTMLworkTitle.replace("%data%", job.title);
             var formattedEmployerTitle= formattedEmployer + formattedTitle;
 
             $(".work-entry:last").append(formattedEmployerTitle);
 
-            var formattedDates = HTMLworkDates.replace("%data%", this.jobs[job].dates);
+            var formattedDates = HTMLworkDates.replace("%data%", job.dates);
             $(".work-entry:last").append(formattedDates);
 
-            var formattedDescription = HTMLworkDescription.replace("%data%", this.jobs[job].description);
+            var formattedDescription = HTMLworkDescription.replace("%data%", job.description);
             $(".work-entry:last").append(formattedDescription);
-        }
+
+        })
     }
 };
 
@@ -187,25 +188,25 @@ var projects = {
         }
     ],
     display: function () {
-        for (var project in this.projects) {
+        this.projects.forEach(function (project) {
             $("#projects").append(HTMLprojectStart);
 
-            var formmattedTitle = HTMLprojectTitle.replace("%data%", this.projects[project].title);
+            var formmattedTitle = HTMLprojectTitle.replace("%data%", project.title);
             $(".project-entry:last").append(formmattedTitle);
 
-            var formattedDate = HTMLprojectDates.replace("%data%", this.projects[project].dates);
+            var formattedDate = HTMLprojectDates.replace("%data%", project.dates);
             $(".project-entry:last").append(formattedDate);
 
-            var formattedDescription = HTMLprojectDescription.replace("%data%", this.projects[project].description);
+            var formattedDescription = HTMLprojectDescription.replace("%data%", project.description);
             $(".project-entry:last").append(formattedDescription);
 
-            if (this.projects[project].images.length > 0) {
-                for (var image in this.projects[project].images) {
-                    var formattedImage = HTMLprojectImage.replace("%data%", this.projects[project].images[image]);
+            if (project.images.length > 0) {
+                project.images.forEach(function (image) {
+                    var formattedImage = HTMLprojectImage.replace("%data%", image);
                     $(".project-entry:last").append(formattedImage);
-                }
+                });
             }
-        }
+        });
     }
 };
 
